02ddad4dae332606ee16177008a5ffe141ace66f
Luka Matic <maticluka888@gmail.com>
2023-08-01 14:56:49 -0700

    real initial commit

---
+++ src/main/java/com/kalumatic/employeesrestcrudapi/EmployeesRestCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.kalumatic.employeesrestcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesRestCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesRestCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalumatic/employeesrestcrudapi/EmployeesRestCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalumatic.employeesrestcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesRestCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
